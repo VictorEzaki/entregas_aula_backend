891ecd8e6367886b0b858965f8f7617ca5eed5b8
Victor <victorezaki19@gmail.com>
2026-04-28 21:47:02 -0300

    ajuste na nomeação das rotas e config comando iniciar projeto

--- src/app.js
+++ src/app.js
@@ -5,15 +5,15 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/expenses", ExpenseView.create);
-app.get("/expenses", ExpenseView.getAll);
-app.get("/expenses/:id", ExpenseView.getById);
-app.put("/expenses/:id", ExpenseView.update);
-app.delete("/expenses/:id", ExpenseView.delete);
+app.post("/api/expenses", ExpenseView.create);
+app.get("/api/expenses", ExpenseView.getAll);
+app.get("/api/expenses/:id", ExpenseView.getById);
+app.put("/api/expenses/:id", ExpenseView.update);
+app.delete("/api/expenses/:id", ExpenseView.delete);
 
 // Extras
-app.get("/expenses/summary/total", ExpenseView.getTotalExpenses);
-app.get("/expenses/summary/category", ExpenseView.getTotalExpensesByCategory);
+app.get("/api/expenses/summary/total", ExpenseView.getTotalExpenses);
+app.get("/api/expenses/summary/category", ExpenseView.getTotalExpensesByCategory);
 app.listen(3000, () => {
   console.info(`Servidor rodando na porta ${3000}`);
 });
